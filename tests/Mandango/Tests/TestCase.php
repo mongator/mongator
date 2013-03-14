62e1740c378b8187a5fee3d9c1dab14975f276b7
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-03-14 18:18:42 +0100

    another travis try

--- tests/Mandango/Tests/TestCase.php
+++ tests/Mandango/Tests/TestCase.php
@@ -39,6 +39,9 @@ class TestCase extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
+        if (!class_exists('Mongo')) {
+            $this->markTestSkipped('Mongo is not available.');
+        }
 
         if (!static::$staticConnection) {
             static::$staticConnection = new Connection($this->server, $this->dbName);
