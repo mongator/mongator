61f4db59908e6d6da0883726f733e91706aa68e3
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-04-17 08:54:03 +0200

    travis fixes

--- .travis.sh
+++ .travis.sh
@@ -2,7 +2,3 @@
 wget https://github.com/nicolasff/phpredis/archive/master.zip
 unzip master.zip
 sh -c "cd phpredis-master && phpize && ./configure && sudo make install"
-
-echo "extension=redis.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-echo "extension=memcached.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-echo "extension=apc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
